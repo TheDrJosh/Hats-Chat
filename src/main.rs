f9415e942318e7fbc52f0253caa1764bbb3dd654
Joshua <josh.a.roo2004@gmail.com>
2023-07-04 00:27:55 -0700

    blank web page

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use std::net::SocketAddr;
 
 use axum::{Router, response::Html, routing::get};
+use tower_http::{
+    services::ServeDir,
+};
+use tracing_subscriber::prelude::*;
 
+const HTML_BASE: &str = include_str!("../assets/base.html");
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+    .with(
+        tracing_subscriber::EnvFilter::try_from_default_env()
+            .unwrap_or_else(|_| "web_chat_app=debug,tower_http=debug".into()),
+    )
+    .with(tracing_subscriber::fmt::layer())
+    .init();
 
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new().route("/", get(handler)).nest_service("/assets", ServeDir::new("assets/"));
     
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("listening on {}", addr);
@@ -16,6 +28,5 @@ async fn main() {
 }
 
 async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+    Html(HTML_BASE)
 }
-
